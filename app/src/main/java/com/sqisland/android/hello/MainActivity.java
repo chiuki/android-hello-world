adc20462b78d109c7b4bec73dd50b0920fa99543
Chiu-Ki Chan <chiuki@gmail.com>
2015-11-18 14:48:44 -0700

    Add menu

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -2,11 +2,32 @@ package com.sqisland.android.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+  private TextView greetingView;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    greetingView = (TextView) findViewById(R.id.greeting);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == R.id.action_bye) {
+      greetingView.setText(R.string.bye);
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
