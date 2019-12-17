bdfae9d0c7a7741ba9b9c619567ef19a578f4959
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2019-12-17 12:01:51 -0700

    Upgrade gradle (6.0.1), gradle plugin (3.5.3), build tools (29.0.2) and compile SDK (29)

--- app/src/androidTest/java/com/sqisland/android/hello/MainActivityTest.java
+++ app/src/androidTest/java/com/sqisland/android/hello/MainActivityTest.java
@@ -1,22 +1,28 @@
 package com.sqisland.android.hello;
 
-import android.test.ActivityInstrumentationTestCase2;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-  public MainActivityTest() {
-    super(MainActivity.class) ;
-  }
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+  @Rule
+  public ActivityTestRule<MainActivity> activityTestRule
+      = new ActivityTestRule<>(MainActivity.class);
+
+  @Test
   public void testBye() {
-    getActivity();
-
     onView(withId(R.id.greeting))
         .check(matches(withText(R.string.hello_world)));
 
